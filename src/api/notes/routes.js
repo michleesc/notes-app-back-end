35b419366a53224710146687a97f96927ba1b688
michleesc <michlee.septian.c@gmail.com>
2022-03-22 11:24:35 +0700

    authentication implemented

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -3,27 +3,41 @@ const routes = (handler) => [
         method: 'POST',
         path: '/notes',
         handler: handler.postNoteHandler, // hanya menerima dan menyimpan "satu" note.
+        options: {
+            auth: 'notesapp_jwt',
+        },
     },
     {
         method: 'GET',
         path: '/notes',
         handler: handler.getNotesHandler, // mengembalikan "banyak" data.
+        options: {
+            auth: 'notesapp_jwt',
+        },
     },
     {
         method: 'GET',
         path: '/notes/{id}',
         handler: handler.getNoteByIdHandler, // mengembalikan "satu" note.
-
+        options: {
+            auth: 'notesapp_jwt',
+        },
     },
     {
         method: 'PUT',
         path: '/notes/{id}',
         handler: handler.putNoteByIdHandler, // hanya menerima dan mengubah "satu" note.
+        options: {
+            auth: 'notesapp_jwt',
+        },
     },
     {
         method: 'DELETE',
         path: '/notes/{id}',
         handler: handler.deleteNoteByIdHandler, // hanya menerima dan menghapus "satu" note.
+        options: {
+            auth: 'notesapp_jwt',
+        },
     },
 ];
 
--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 // menggunakan framework hapi.js
 const Hapi = require('@hapi/hapi');
 
+// JWT
+const Jwt = require('@hapi/jwt');
+
 // notes
 const notes = require('./api/notes');
 const NotesService = require('./service/postgres/NotesService');
@@ -14,9 +17,16 @@ const users = require('./api/users');
 const UsersService = require('./service/postgres/UsersService');
 const UsersValidator = require('./validator/users');
 
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./service/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
+
 const init = async () => {
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -25,6 +35,30 @@ const init = async () => {
         origin: ['*'], // enable CORS
       },
     },
+  });
+  
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+  
+  // mendefinisikan strategy autentikasi jwt
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
   
   await server.register([
@@ -40,6 +74,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
